cbb57d25108accbb16640ffb2f2c2a0ee719b794
sachinrijal12 <rijalsachin58@gmail.com>
2026-02-06 11:00:45 +0545

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return <React.Fragment>
+    <h1>Healthy Food</h1>
+    <ul class="list-group">
+      <li class="list-group-item">Dal</li>
+      <li class="list-group-item">Grenn vegetable</li>
+      <li class="list-group-item">Roti</li>
+      <li class="list-group-item">Salad</li>
+      <li class="list-group-item">water</li>
+    </ul>
+  </React.Fragment>
+}
+export default App;
